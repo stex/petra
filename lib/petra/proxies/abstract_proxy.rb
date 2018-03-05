f42f6f37e59469ba55255093af12c2ac40cc648b
Stefan Exner <stex@sterex.de>
2018-03-05 21:33:23 +0100

    refactored abstract_proxy class methods

--- lib/petra/proxies/abstract_proxy.rb
+++ lib/petra/proxies/abstract_proxy.rb
@@ -4,78 +4,90 @@ module Petra
   module Proxies
     class AbstractProxy
 
-      protected
+      class << self
+        #
+        # Builds an ObjectProxy for the given object.
+        # If a more specific proxy class exists for the given object,
+        # it will be used instead of the generic Petra::Proxies::ObjectProxy.
+        #
+        # If there is no proxy for the exact class of the given +object+,
+        # its superclasses are automatically tested.
+        #
+        def for(object, inherited: false, **options)
+          # If the given object is configured not to use a possibly existing
+          # specialized proxy (e.g. the ActiveRecord::Base proxy), we simply
+          # build a default ObjectProxy for it, but we'll still try to extend it using
+          # available ModuleProxies
+          default_proxy = ObjectProxy.new(object, inherited, **options)
+          default_proxy.send :mixin_module_proxies!
+          return default_proxy unless inherited_config_for(object, :use_specialized_proxy)
 
-      #
-      # Determines the available object proxy classes and the ruby classes they
-      # can be used for. All classes in the Petra::Proxies namespace are automatically
-      # recognized as long as they define a CLASS_NAMES constant.
-      #
-      # If multiple proxies specify the same class name, the last one by sorting wins.
-      #
-      # @return [Hash] The available proxy classes in the format ("ClassName" => "ProxyClassName")
-      #
-      def self.available_class_proxies
-        @class_proxies ||= Petra::Proxies.constants.each_with_object({}) do |c, h|
-          klass = Petra::Proxies.const_get(c)
-          # Skip non-class constants (this includes modules)
-          next unless klass.is_a?(Class)
-          # Skip every class which is not an ObjectProxy. There shouldn't be any
-          # in this namespace, but you never know...
-          next unless klass <= Petra::Proxies::ObjectProxy
-          # Skip proxy classes which do not specify which classes
-          # they were built for
-          next unless klass.const_defined?(:CLASS_NAMES)
+          # Otherwise, we search for a specialized proxy for the object's class
+          # and its superclasses until we either find one or reach the
+          # default ObjectProxy
+          klass = object.is_a?(Class) ? object : object.class
+          klass = klass.superclass until available_class_proxies.key?(klass.to_s)
+          proxy = available_class_proxies[klass.to_s].constantize.new(object, inherited, **options)
 
-          klass.const_get(:CLASS_NAMES).each { |n| h[n] = "Petra::Proxies::#{c}" }
+          # If we reached Object, we might still find one or more ModuleProxy module we might
+          # mix into the resulting ObjectProxy. Otherwise, the specialized proxy will most likely
+          # have included the necessary ModuleProxies itself.
+          proxy.send(:mixin_module_proxies!) if proxy.instance_of?(Petra::Proxies::ObjectProxy)
+          proxy
         end
-      end
 
-      #
-      # @see #available_class_proxies
-      #
-      # Returns only module proxies
-      #
-      def self.available_module_proxies
-        @module_proxies ||= Petra::Proxies.constants.each_with_object({}) do |c, h|
-          klass = Petra::Proxies.const_get(c)
-          next unless klass.is_a?(Module)
-          next unless klass.included_modules.include?(Petra::Proxies::ModuleProxy)
-          next unless klass.const_defined?(:MODULE_NAMES)
+        #
+        # Determines the available object proxy classes and the ruby classes they
+        # can be used for. All classes in the Petra::Proxies namespace are automatically
+        # recognized as long as they define a CLASS_NAMES constant.
+        #
+        # If multiple proxies specify the same class name, the last one by sorting wins.
+        #
+        # @return [Hash] The available proxy classes in the format ("ClassName" => "ProxyClassName")
+        #
+        def available_class_proxies
+          @class_proxies ||= Petra::Proxies.constants.each_with_object({}) do |c, h|
+            klass = Petra::Proxies.const_get(c)
+            # Skip non-class constants (this includes modules)
+            next unless klass.is_a?(Class)
+            # Skip every class which is not an ObjectProxy. There shouldn't be any
+            # in this namespace, but you never know...
+            next unless klass <= Petra::Proxies::ObjectProxy
+            # Skip proxy classes which do not specify which classes
+            # they were built for
+            next unless klass.const_defined?(:CLASS_NAMES)
 
-          klass.const_get(:MODULE_NAMES).each { |n| h[n] = "Petra::Proxies::#{c}" }
+            klass.const_get(:CLASS_NAMES).each { |n| h[n] = "Petra::Proxies::#{c}" }
+          end
         end
-      end
 
-      #
-      # Builds an ObjectProxy for the given object.
-      # If a more specific proxy class exists for the given object,
-      # it will be used instead of the generic Petra::Proxies::ObjectProxy.
-      #
-      # If there is no proxy for the exact class of the given +object+,
-      # its superclasses are automatically tested.
-      #
-      def self.for(object, inherited: false, **options)
-        # If the given object is configured not to use a possibly existing
-        # specialized proxy (e.g. the ActiveRecord::Base proxy), we simply
-        # build a default ObjectProxy for it, but we'll still try to extend it using
-        # available ModuleProxies
-        default_proxy = ObjectProxy.new(object, inherited, **options)
-        default_proxy.send :mixin_module_proxies!
-        return default_proxy unless inherited_config_for(object, :use_specialized_proxy)
+        #
+        # @see #available_class_proxies
+        #
+        # Returns only module proxies
+        #
+        def available_module_proxies
+          @module_proxies ||= Petra::Proxies.constants.each_with_object({}) do |c, h|
+            klass = Petra::Proxies.const_get(c)
+            next unless klass.is_a?(Module)
+            next unless klass.included_modules.include?(Petra::Proxies::ModuleProxy)
+            next unless klass.const_defined?(:MODULE_NAMES)
 
-        # Otherwise, we search for a specialized proxy for the object's class
-        # and its superclasses until we either find one or reach the
-        # default ObjectProxy
-        klass = object.is_a?(Class) ? object : object.class
-        klass = klass.superclass until available_class_proxies.key?(klass.to_s)
-        proxy = available_class_proxies[klass.to_s].constantize.new(object, inherited, **options)
+            klass.const_get(:MODULE_NAMES).each { |n| h[n] = "Petra::Proxies::#{c}" }
+          end
+        end
 
-        # If we reached Object, we might still find one or more ModuleProxy module we might
-        # mix into the resulting ObjectProxy. Otherwise, the specialized proxy will most likely
-        # have included the necessary ModuleProxies itself.
-        proxy.send(:mixin_module_proxies!) if proxy.instance_of?(Petra::Proxies::ObjectProxy)
-        proxy
+        #
+        # Retrieves a configuration value with the given name respecting
+        # custom configurations made for its class (or class family)
+        #
+        def inherited_config_for(object, name, *args)
+          # If the proxied object already is a class, we don't use its class (Class)
+          # as there is a high chance nobody will ever use this object proxy on
+          # this level of meta programming
+          klass = object.is_a?(Class) ? object : object.class
+          Petra.configuration.class_configurator(klass).__inherited_value(name, *args)
+        end
       end
 
       #
@@ -118,18 +130,6 @@ module Petra
       #
       def transaction
         Petra.transaction_manager.current_transaction
-      end
-
-      #
-      # Retrieves a configuration value with the given name respecting
-      # custom configurations made for its class (or class family)
-      #
-      def self.inherited_config_for(object, name, *args)
-        # If the proxied object already is a class, we don't use its class (Class)
-        # as there is a high chance nobody will ever use this object proxy on
-        # this level of meta programming
-        klass = object.is_a?(Class) ? object : object.class
-        Petra.configuration.class_configurator(klass).__inherited_value(name, *args)
       end
 
       #
